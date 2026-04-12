3896037a5b404bb30eaa39e8fd9fa89cf5cbd717
mtmn <miro@haravara.org>
2026-04-12 23:01:12 +0200

    fix: get rid of regex interop functions

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,3 @@ import { config } from 'dotenv';
 export const dotenvConfig = () => {
   config();
 };
-
-export const writeBuffer = (stream) => (buffer) => () => {
-  stream.write(Buffer.from(buffer));
-};
-
-export const sanitizeKey = (str) => {
-  return str.replace(/[^a-z0-9.-]/gi, '_').replace(/_{2,}/g, '_');
-};
