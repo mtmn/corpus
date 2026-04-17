4824c5183b7569c0b6832e17bf2997f844cca7a8
mtmn <miro@haravara.org>
2026-04-17 13:18:54 +0200

    feat: use elm in frontend instead of halogen

--- src/Client.js
+++
@@ -1,8 +0,0 @@
-export const extractParam = (key) => (search) => {
-	const params = new URLSearchParams(search);
-	return params.get(key);
-};
-
-export const formatRFC3339 = (instant) => {
-	return new Date(instant).toISOString();
-};
